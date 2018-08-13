17e2355f31e2a3195b7657e790951d2ea6b63f4d
Dipesh Thapa <dpesofficial@gmail.com>
2018-08-13 13:48:14 +0545

    footer link supdated

--- wp-content/themes/calderflower/template-parts/content.php
+++ wp-content/themes/calderflower/template-parts/content.php
@@ -184,12 +184,16 @@
     <div class="copyright-block">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-7">
                 <?php $copyright = get_theme_mod( 'footer_copyright_setting' );?>
                 <p><?php echo $copyright;?></p>
+                <ul class="footer-links">
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                </ul>
             </div>
 
-             <div class="col-lg-6 d-flex justify-content-end align-items-center">
+             <div class="col-lg-5 d-flex justify-content-end align-items-center">
                 <?php cf_social_icons_output(); ?>
             </div>
             </div>
