b13defbcd32df671fae13b197fdb4b647586cd91
Dipesh Thapa <dpesofficial@gmail.com>
2018-08-21 15:23:18 +0545

    milestone cat css update

--- wp-content/themes/calderflower/template-parts/template-milestones.php
+++ wp-content/themes/calderflower/template-parts/template-milestones.php
@@ -59,142 +59,104 @@ Template Name: Milestones
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
                 </a>
 			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-picture">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg" alt="Picture">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Title of section 3</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, obcaecati, quisquam id molestias eaque asperiores voluptatibus cupiditate error assumenda delectus odit similique earum voluptatem doloremque dolorem ipsam quae rerum quis. Odit, itaque, deserunt corporis vero ipsum nisi eius odio natus ullam provident pariatur temporibus quia eos repellat consequuntur perferendis enim amet quae quasi repudiandae sed quod veniam dolore possimus rem voluptatum eveniet eligendi quis fugiat aliquam sunt similique aut adipisci.</p>
-				<a href="#0" class="cd-read-more">Read more</a>
-				<span class="cd-date">Jan 24</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-location">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location.svg" alt="Location">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Title of section 4</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hic quasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis qui ut.</p>
-				<a href="#0" class="cd-read-more">Read more</a>
-				<span class="cd-date">Feb 14</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-location">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-location.svg" alt="Location">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Title of section 5</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum.</p>
-				<a href="#0" class="cd-read-more">Read more</a>
-				<span class="cd-date">Feb 18</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-
-		<div class="cd-timeline-block">
+        </div> <!-- cd-timeline-block -->
+        
+		<div class="cd-timeline-block cf-updates">
 			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
 			</div> <!-- cd-timeline-img -->
 
 			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
+				<a href="#" class="milestone-blk">
+                    <div class="mil-content">
+                        <span class="mil-date">August 17, 2015</span>
+                        <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
+                        <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
+                    </div>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
+                </a>
 			</div> <!-- cd-timeline-content -->
         </div> <!-- cd-timeline-block -->
         
-		<div class="cd-timeline-block">
+		<div class="cd-timeline-block cf-updates">
 			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
 			</div> <!-- cd-timeline-img -->
 
 			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
+				<a href="#" class="milestone-blk">
+                    <div class="mil-content">
+                        <span class="mil-date">August 17, 2015</span>
+                        <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
+                        <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
+                    </div>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
+                </a>
 			</div> <!-- cd-timeline-content -->
 		</div> <!-- cd-timeline-block -->
+
+        <div class="cd-timeline-block awards-ach">
+			<div class="cd-timeline-img cd-movie">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<a href="#" class="milestone-blk">
+                    <div class="mil-content">
+                        <span class="mil-date">August 17, 2015</span>
+                        <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
+                        <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
+                    </div>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
+                </a>
+			</div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
+
+        <div class="cd-timeline-block mil-articles">
+			<div class="cd-timeline-img cd-movie">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<a href="#" class="milestone-blk">
+                    <div class="mil-content">
+                        <span class="mil-date">August 17, 2015</span>
+                        <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
+                        <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
+                    </div>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
+                </a>
+			</div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
         
-		<div class="cd-timeline-block">
+        <div class="cd-timeline-block mil-articles">
 			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
 			</div> <!-- cd-timeline-img -->
 
 			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
+				<a href="#" class="milestone-blk">
+                    <div class="mil-content">
+                        <span class="mil-date">August 17, 2015</span>
+                        <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
+                        <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
+                    </div>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
+                </a>
 			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
+        </div> <!-- cd-timeline-block -->
+
+        <div class="cd-timeline-block case-studies">
+			<div class="cd-timeline-img cd-picture">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<a href="#" class="milestone-blk">
+                    <div class="mil-content">
+                        <span class="mil-date">January 20, 2016</span>
+                        <span class="mil-title">Ghana Hospital opens for business</span>
+                        <p>The new rural hospital in Ghana desigend by Calder Flower has opened it's doors to the first patients. Construction will continue on the next stages but the local community are already on board with their new health facility. </p>
+                    </div>
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
+                </a>
+			</div> <!-- cd-timeline-content -->
+        </div> <!-- cd-timeline-block -->
         
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-        
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-        
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-        
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
-        
-		<div class="cd-timeline-block">
-			<div class="cd-timeline-img cd-movie">
-				<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-movie.svg" alt="Movie">
-			</div> <!-- cd-timeline-img -->
-
-			<div class="cd-timeline-content">
-				<h2>Final Section</h2>
-				<p>This is the content of the last section</p>
-				<span class="cd-date">Feb 26</span>
-			</div> <!-- cd-timeline-content -->
-		</div> <!-- cd-timeline-block -->
 	</section>
 
  <?php
