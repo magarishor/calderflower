8bef1fc931edd5a13c598cbfd2e2bdf2494f59c9
Ishor Magar <alemagarishor@gmail.com>
2018-08-28 14:26:41 +0545

    milestone

--- wp-content/themes/calderflower/template-milestones.php
+++ wp-content/themes/calderflower/template-milestones.php
@@ -36,152 +36,65 @@ Template Name: Milestones
     ── -->
     <div class="container milestone-timeline">
         <div class="mil-filter">
-                <select id="project-area" class="selectric sel-white" tabindex="0">
-                    <option value="">Filter milestones by category</option>
-                    <option value="case-studies" class="case-studies">case studies</option>
-                    <option value="awards"  class="awards">awards & Achievements</option>
-                    <option value="cf-updates"  class="cf-updates">cf updates</option>
-                    <option value="articles"  class="articles">articles</option>
-                </select>
+            <select id="milestone" class="selectric sel-white" tabindex="0">
+                <option value="">Filter milestones by category</option>
+                <?php
+                   $args = array(
+                               'taxonomy' => 'cfcategory_type',
+                               'orderby' => 'name',
+                               'order'   => 'ASC'
+                           );
+
+                   $cats = get_categories($args);
+                    foreach( $cats as $cat ) {
+                    ?>
+                <option value="<?php echo $cat->slug;?>" class="<?php echo $cat->slug;?>"><?php echo $cat->name;?></option>
+                <?php }?>
+            </select>
         </div>
 
         <section id="cd-timeline" class="cd-container cssanimations">
-            <div class="cd-timeline-block case-studies">
+    	<?php
+	          $args = array(
+	              'post_type'=>'milestone',
+	              'orderby'  => 'date',
+	              'order' => 'ASC',
+	              'posts_per_page' => -1
+	          );
+	          $query_milestone = new WP_Query( $args );
+	          while ( $query_milestone->have_posts() ) :
+	          $query_milestone->the_post();
+	          $feature_img = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );
+	          $term_list = wp_get_post_terms(get_the_ID(), 'cfcategory_type', array("fields" => "all"));
+	          $term_slug = $term_list[0]->slug;
+            ?>
+            <div class="cd-timeline-block <?php echo $term_slug;?>">
                 <div class="cd-timeline-img cd-picture">
                 </div> <!-- cd-timeline-img -->
 
                 <div class="cd-timeline-content">
                     <a href="#" class="milestone-blk">
                         <div class="mil-content">
-                            <span class="mil-date">January 20, 2016</span>
-                            <span class="mil-title">Ghana Hospital opens for business</span>
-                            <p>The new rural hospital in Ghana desigend by Calder Flower has opened it's doors to the first patients. Construction will continue on the next stages but the local community are already on board with their new health facility. </p>
+                            <span class="mil-date"><?php echo get_the_date('F d, Y');?></span>
+                            <span class="mil-title"><?php the_title();?></span>
+                            <?php the_content();?>
                         </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
+                        <img src="<?php echo $feature_img;?>" alt="<?php the_title();?>">
                     </a>
                 </div> <!-- cd-timeline-content -->
             </div> <!-- cd-timeline-block -->
-
-            <div class="cd-timeline-block awards-ach">
-                <div class="cd-timeline-img cd-movie">
-                </div> <!-- cd-timeline-img -->
-
-                <div class="cd-timeline-content">
-                    <a href="#" class="milestone-blk">
-                        <div class="mil-content">
-                            <span class="mil-date">August 17, 2015</span>
-                            <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
-                            <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
-                        </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
-                    </a>
-                </div> <!-- cd-timeline-content -->
-            </div> <!-- cd-timeline-block -->
-
-            <div class="cd-timeline-block cf-updates">
-                <div class="cd-timeline-img cd-movie">
-                </div> <!-- cd-timeline-img -->
-
-                <div class="cd-timeline-content">
-                    <a href="#" class="milestone-blk">
-                        <div class="mil-content">
-                            <span class="mil-date">August 17, 2015</span>
-                            <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
-                            <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
-                        </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
-                    </a>
-                </div> <!-- cd-timeline-content -->
-            </div> <!-- cd-timeline-block -->
-
-            <div class="cd-timeline-block cf-updates">
-                <div class="cd-timeline-img cd-movie">
-                </div> <!-- cd-timeline-img -->
-
-                <div class="cd-timeline-content">
-                    <a href="#" class="milestone-blk">
-                        <div class="mil-content">
-                            <span class="mil-date">August 17, 2015</span>
-                            <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
-                            <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
-                        </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
-                    </a>
-                </div> <!-- cd-timeline-content -->
-            </div> <!-- cd-timeline-block -->
-
-            <div class="cd-timeline-block awards-ach">
-                <div class="cd-timeline-img cd-movie">
-                </div> <!-- cd-timeline-img -->
-
-                <div class="cd-timeline-content">
-                    <a href="#" class="milestone-blk">
-                        <div class="mil-content">
-                            <span class="mil-date">August 17, 2015</span>
-                            <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
-                            <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
-                        </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
-                    </a>
-                </div> <!-- cd-timeline-content -->
-            </div> <!-- cd-timeline-block -->
-
-            <div class="cd-timeline-block mil-articles">
-                <div class="cd-timeline-img cd-movie">
-                </div> <!-- cd-timeline-img -->
-
-                <div class="cd-timeline-content">
-                    <a href="#" class="milestone-blk">
-                        <div class="mil-content">
-                            <span class="mil-date">August 17, 2015</span>
-                            <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
-                            <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
-                        </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
-                    </a>
-                </div> <!-- cd-timeline-content -->
-            </div> <!-- cd-timeline-block -->
-
-            <div class="cd-timeline-block mil-articles">
-                <div class="cd-timeline-img cd-movie">
-                </div> <!-- cd-timeline-img -->
-
-                <div class="cd-timeline-content">
-                    <a href="#" class="milestone-blk">
-                        <div class="mil-content">
-                            <span class="mil-date">August 17, 2015</span>
-                            <span class="mil-title">Calder Flower Architects Wins UDIA Excellence Award and Commendation</span>
-                            <p>CFA received two awards at the 2015 UDIA NSW Awards for Excellence Gala Dinner.  </p>
-                        </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
-                    </a>
-                </div> <!-- cd-timeline-content -->
-            </div> <!-- cd-timeline-block -->
-
-            <div class="cd-timeline-block case-studies">
-                <div class="cd-timeline-img cd-picture">
-                </div> <!-- cd-timeline-img -->
-
-                <div class="cd-timeline-content">
-                    <a href="#" class="milestone-blk">
-                        <div class="mil-content">
-                            <span class="mil-date">January 20, 2016</span>
-                            <span class="mil-title">Ghana Hospital opens for business</span>
-                            <p>The new rural hospital in Ghana desigend by Calder Flower has opened it's doors to the first patients. Construction will continue on the next stages but the local community are already on board with their new health facility. </p>
-                        </div>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/mil-img.jpg" alt="mil first">
-                    </a>
-                </div> <!-- cd-timeline-content -->
-            </div> <!-- cd-timeline-block -->
-
+        <?php
+    		endwhile;
+        	wp_reset_postdata();
+        	?>
         </section>
     </div>
 
  <?php
-        //get_template_part( 'template-parts/home/content', 'letstalk' );?>
+        get_template_part( 'template-parts/home/content', 'letstalk' );?>
 
 <?php
-       // get_template_part( 'template-parts/home/content', 'copyright' );?>
+        get_template_part( 'template-parts/home/content', 'copyright' );?>
 
 <?php
         get_footer(); ?>
