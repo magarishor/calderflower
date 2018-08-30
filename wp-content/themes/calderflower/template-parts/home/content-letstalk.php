088a62e96a3e69f5690c7c33e819286df687cf41
Ishor Magar <alemagarishor@gmail.com>
2018-08-30 08:14:36 +0545

    letstalk title desc issue fixing

--- wp-content/themes/calderflower/template-parts/home/content-letstalk.php
+++ wp-content/themes/calderflower/template-parts/home/content-letstalk.php
@@ -5,10 +5,15 @@
 ── -->
 <div class="form-map">
 	<?php $letstalk = get_field('cf_home_lets_talk' , '9' );?>
+    <?php if(is_front_page()){?>
     <span class="heading-title"><?php echo $letstalk['cf_home_lets_talk_description'];?></span>
+    <?php }?>
     <div class="btn-wrap-center">
+         <?php if(is_front_page()){?>
         <a href="#" class="site-btn btn-green"><?php echo $letstalk['cf_home_lets_talk_title'];?></a>
+        <?php }?>
     </div>
+
     <div class="container">
         <div class="row no-gutters">
 
